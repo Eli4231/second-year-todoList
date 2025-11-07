6009010f245a3cdaf7b22e470f6305b4be2c5fa9
Eli lang <elilang92@gmail.com>
2025-11-07 10:09:41 +0200

    קישרנו DB

---
+++ config/db_config.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    dateStrings: true,
+});
+
+pool.getConnection((err, connection) => {
+    if (err) {
+        console.error('Error connecting to the database: ' , err.message);
+       
+    }else
+    console.log('Connected to the database');
+    connection.release();
+});
+module.exports = pool.promise();
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const path = require('path');
+require('dotenv').config();
+const port = process.env.PORT ;
+const api = process.env.HOST ;
 const app = express();
-const port = 3000;
+app.use(express.static(__dirname + '/public'));
+const db = require('./config/db_config');
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port http://${api}:${port}`);
 });
-app.get('/', (req, res) => {
-    res.send('השרת עובד');
-});
-
